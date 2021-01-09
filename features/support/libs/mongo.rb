695c7aa51763e6cf3f6d974dbb00232e6395c098
Henrique Cervi <henrique.cervi@terra.com.br>
2021-01-08 22:04:30 -0300

    Utilizando initialize.

--- features/support/libs/mongo.rb
+++ features/support/libs/mongo.rb
@@ -4,26 +4,28 @@ Mongo::Logger.logger = Logger.new("./logs/mongo.log")
 
 
 class MongoDB
-    
-    def remove_user(email)
+
+    attr_accessor :users, :equipos #< são as collections do mongo
+
+    def initialize #já inicializa
         client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
-        users = client[:users]
-        users.delete_many({email: email})
+        @users = client[:users]
+        @equipos = client[:equipos] #equipos é o nome da collection do MONGODB que estamos manipulando!
+    end
+    
+    def remove_user(email)       
+        @users.delete_many({email: email})
     end
 
     # criamos esse método para conseguir selecionar o usuário correto, sendo assim não temos um falso positivo.
-    def get_user(email)
-        client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
-        users = client[:users]
-        user = users.find({email: email}).first
+    def get_user(email)        
+        user = @users.find({email: email}).first
         return user[:_id]
     end
 
     def remove_equipo(name, email)
-        user_id = get_user(email)
-        client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
-        equipos = client[:equipos] #equipos é o nome da collection do MONGODB que estamos manipulando!
-        equipos.delete_many({name: name, user: user_id})
+        user_id = get_user(email)        
+        @equipos.delete_many({name: name, user: user_id})
     end
 
 end
