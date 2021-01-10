d30a2323d91edbc5d85bb463be2017d7419f731d
Henrique Cervi <henrique.cervi@terra.com.br>
2021-01-10 14:38:29 -0300

    Continuando à refatoração utilizando Page Objects.

---
+++ features/support/views/components/alert.rb
@@ -0,0 +1,11 @@
+
+class Alert
+   include Capybara::DSL
+
+    def dark
+        return find('.alert-dark').text  #encontrando a classe que estamos buscando.
+        #utilizamos o .text para "puxar" o texto que temos dentro da classe e assim conseguimos efetuar à validação
+    end
+
+
+end
