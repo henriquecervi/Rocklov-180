95662c0f7c44dec8997dc4e1235718b47bc58d9a
Henrique Cervi <henrique.cervi@terra.com.br>
2021-01-12 19:07:39 -0300

    Configurando vários browsers, utilizando CASE e [BROWSER].

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,8 +5,21 @@ require "faker"
 
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["CONFIG"]}"))
 
+case ENV["BROWSER"]
+when "firefox"
+  @diver = :selenium
+when "chrome"
+  @driver = :selenium_chrome
+when "fire_headless"
+  @driver = :selenium_headless
+when "chrome_headless"
+  @driver = :selenium_chrome_headless
+else
+  raise "Navegador inválido. Variável @driver não definida." #exception
+end
+
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  config.default_driver = @driver
   config.app_host = CONFIG["url"]
   config.default_max_wait_time = 15
 end
