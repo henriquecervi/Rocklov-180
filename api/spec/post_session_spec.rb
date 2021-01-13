ab475031551d1e8f2d71603078cff838dd7f1093
Henrique Cervi <henrique.cervi@terra.com.br>
2021-01-12 21:08:32 -0300

    Utilizando Before e Contexto.

--- api/spec/post_session_spec.rb
+++ api/spec/post_session_spec.rb
@@ -2,17 +2,25 @@ require "httparty"
 
 #suit
 describe "POST /sessions" do
-  it "login com sucesso" do
-    payload = { email: "henrique@gmail.com.br", password: "pwd123" }
-    result = HTTParty.post(
-      "http://rocklov-api:3333/sessions",
-      body: payload.to_json,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    )
-    expect(result.code).to eql 200
-    expect(result.parsed_response["_id"].length).to eql 24
-    #usamos o parsed para transformar em hash e o 24 é o resultado que o mongo sempre vai retornar
+  context "login com sucesso" do
+    before(:all) do
+      payload = { email: "henrique@gmail.com.br", password: "pwd123" }
+      @result = HTTParty.post(
+        "http://rocklov-api:3333/sessions",
+        body: payload.to_json,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      )
+    end
+    it "valida status code" do
+      expect(@result.code).to eql 200
+    end
+
+    it "valida id do usuario" do
+      expect(@result.parsed_response["_id"].length).to eql 24
+      #usamos o parsed para transformar em hash e o 24 é o resultado que o mongo sempre vai retornar
+
+    end
   end
 end
