50197845ef133992ca066b256a17a03ea15792b1
Henrique Cervi <henrique.cervi@terra.com.br>
2021-01-14 21:27:08 -0300

    Validando a rota Signup completa, utilizando arquivo YML.

--- api/spec/post_signup_spec.rb
+++ api/spec/post_signup_spec.rb
@@ -1,9 +1,12 @@
 require_relative "routes/signup"
+require_relative "libs/mongo"
+require_relative "helpers"
 
 describe "POST /signup" do
   context "novo usuario" do
     before(:all) do
       payload = { name: "Bruna", email: "bruna@gmail.com", password: "pwd123" }
+      MongoDB.new.remove_user(payload[:email])
 
       @result = Signup.new.create(payload)
     end
@@ -14,6 +17,42 @@ describe "POST /signup" do
 
     it "valida id do usuário" do
       expect(@result.parsed_response["_id"].length).to eql 24
+    end
+  end
+
+  context "usuario ja existe" do
+    before(:all) do
+      payload = { name: "Cida", email: "cida@gmail.com", password: "pwd123" }
+      MongoDB.new.remove_user(payload[:email])
+
+      Signup.new.create(payload)
+      @result = Signup.new.create(payload)
+    end
+    it "valida status code 409" do
+      expect(@result.code).to eql 409
+    end
+    it "valida msg de erro" do
+      expect(@result.parsed_response["error"]).to eql "Email already exists :("
+    end
+  end
+
+  examples = Helpers::get_fixtures("signup")
+  # fizemos o require relative no começo do arquivo para poder trabalhar com o Helpers e
+  # na sequência utilizamos o modulo que criamos, agora se tivermos outra massa em outro arquivo
+  # é só puxar pelo outro nome ex: examples = Helpers::get_fixtures("cadastro")
+
+  examples.each do |e|
+    context "#{e[:title]}" do
+      before(:all) do
+        @result = Signup.new.create(e[:payload])
+      end
+      it "valida status code #{e[:code]}" do
+        expect(@result.code).to eql e[:code]
+      end
+
+      it "valida msg de retorno" do
+        expect(@result.parsed_response["error"]).to eql e[:error]
+      end
     end
   end
 end
